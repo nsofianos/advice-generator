4a46bd0818b0f3f85ee927cd1fc99259191d1e2d
nsofianos <nsofianos@hotmail.com>
2022-07-06 15:56:57 -0700

    fetchquotes script updates quote/advice id

--- public/scripts/fetchQuotes.js
+++ public/scripts/fetchQuotes.js
@@ -1,16 +1,23 @@
 const adviceApiURL = "https://api.adviceslip.com/advice";
 
 window.onload = () => {
+  const rerollButtonElement = document.getElementById("advice-reroll");
+  const quoteElement = document.getElementById("advice-quote");
+  const adviceNumberElement = document.getElementById("advice-number");
+
+  const updateAdvice = (data) => {
+    quoteElement.innerHTML = `"${data.advice}"`;
+    adviceNumberElement.innerHTML = `ADVICE # ${data.id}`;
+  };
+
   const fetchAdvice = (url) => {
     fetch(url)
       .then((response) => response.json())
-      .then((data) => console.log(data.slip))
+      .then((data) => updateAdvice(data.slip))
       .catch((error) => console.log(error));
   };
 
-  const rerollButtonElement = document.getElementById("advice-reroll");
-
-  rerollButtonElement.addEventListener("click", () =>
-    fetchAdvice(adviceApiURL)
-  );
+  rerollButtonElement.addEventListener("click", () => {
+    fetchAdvice(adviceApiURL);
+  });
 };
